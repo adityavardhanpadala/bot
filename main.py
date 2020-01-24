2d9b3e0a5fd11f7d4a49d489617f27a08c466473
Aditya Vardhan <44111127+adityavardhanpadala@users.noreply.github.com>
2020-01-24 15:45:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ TYPE, REASON = range(2)
 
 def start(bot, context):
     user = bot.message.from_user
-    bot.message.reply_text("Hi @" + user['username'] + "!\n I am chowkidar of amFOSS.")
+    bot.message.reply_text("Hi @" + user['username'] + "!\n I am R2D2 of bi0s.")
     bot.message.reply_text("/help - for more details")
 
 def help(bot, context):
